77df720347fb094fbe5376f4a17682fc84587f67
James Grogan <james.grogan2@gmail.com>
2022-10-14 13:02:43 +0100

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,25 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Image from "next/image";
 import { useEffect } from "react";
+import Script from "next/script";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-ZQG9EXSZ30`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ZQG9EXSZ30', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script>
       <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
         <div className="px-6 w-full flex flex-wrap items-center justify-between">
           <div className="flex items-center">
