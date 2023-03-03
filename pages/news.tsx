da82c0523a8ba55c0922afcf8cb0ac514889ba23
James Grogan <james.grogan2@gmail.com>
2023-03-03 09:48:22 +0000

    Update news.tsx

--- pages/news.tsx
+++ pages/news.tsx
@@ -6,7 +6,7 @@ const News: NextPage = () => {
   return (
     <main className="max-w-[1200px] m-auto p-4">
       <div className="pt-10 md:pt-20 text-center max-w-3xl m-auto text-black font-bold font-impact">
-        <h1 className=" text-2xl md:text-3xl ">The Health Business Leader</h1>
+        <h1 className=" text-2xl md:text-3xl ">The Healthy Business Leader</h1>
         <h2 className=" md:text-5xl text-4xl leading-[1.2] mt-2">
           {`IMPROVE YOUR HEALTH & INCREASE BUSINESS PERFORMANCE FAST`}
         </h2>
