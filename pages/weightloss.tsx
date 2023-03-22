05641e618de1ff5a86742701ea90f8564205bc2d
James Grogan <james.grogan@arup.com>
2023-03-22 16:32:24 +0000

    add weightloss img

--- pages/weightloss.tsx
+++ pages/weightloss.tsx
@@ -55,6 +55,7 @@ const Weightloss: NextPage = () => {
         />
         <meta property="og:url" content="https://truthfitness.co.uk/" />
         <meta property="og:site_name" content="TRUTH Fitness" />
+        <meta property="og:image" content="weightloss-og-image.jpg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
