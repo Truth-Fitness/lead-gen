c69fe9a9e4f00fad6c87abf1855fe16f68e248a0
James Grogan <james.grogan2@gmail.com>
2023-04-18 18:48:04 +0100

    small changes to people

--- components/CalendlyLink.tsx
+++ components/CalendlyLink.tsx
@@ -4,7 +4,7 @@ export const CalendlyLink = () => {
       href="https://calendly.com/truth-fit/initialcall"
       className="inline-block px-6 py-2.5 text-white bg-gradient-to-tl from-navy to-turq hover:to-turq-dark rounded-lg shadow-lg font-bold text-lg leading-tight uppercase  focus:outline-none focus:ring-0 transition duration-150 ease-in-out cursor-pointer"
     >
-      Book Now
+      Book a call
     </a>
   );
 };
