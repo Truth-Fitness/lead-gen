01d19a70ac67a0e58d934522293cd3ffc4c550a4
James Grogan <james.grogan2@gmail.com>
2025-03-26 11:48:28 +0000

    fix url

--- components/blog/PostPreview.tsx
+++ components/blog/PostPreview.tsx
@@ -23,7 +23,7 @@ export default function PostPreview({ post }: { post: Blog }) {
         )}
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
-        <Link href={`/posts/${post.sys.id}`} className="hover:underline">
+        <Link href={`/blog/${post.sys.id}`} className="hover:underline">
           {post.fields.title}
         </Link>
       </h3>
